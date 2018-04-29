1f34e43ad38c8894f0583a1fc7d2f41a5224d064
disc99 <disc99.g@gmail.com>
2018-04-29 23:32:37 +0900

    Add styles

--- ui/src/SpeackerDeckSlider.js
+++ ui/src/SpeackerDeckSlider.js
@@ -4,6 +4,33 @@ import axios from 'axios'
 
 axios.defaults.baseURL = 'https://6nmkgbb8xj.execute-api.ap-northeast-1.amazonaws.com';
 
+const styles = {
+  position: "relative",
+  textAlign: "center",
+  maxWidth:"100%",
+  maxHeight:"100%"
+};
+
+const styles2 = {
+  position: "absolute",
+  top: -4,
+  right: 0,
+  left: 0,
+  background: "#0002",
+  maxWidth:"100%",
+};
+
+const styles3 = {
+  marginLeft: 4,
+  color: "#777",
+  width:"100%",
+  left: "30px",
+  border: "none",
+  borderBottom: "1px solid #3331",
+  backgroundColor: "transparent"
+};
+
+
 export default class SpeackerDeckSlider extends Component {
   constructor(props) {
     super(props);
@@ -25,30 +52,28 @@ export default class SpeackerDeckSlider extends Component {
       }));
     })
     .catch(function (error) {
-      console.log(error);
+      console.error(error);
     })
   }
 
   render() {
     const settings = {
-      dots: true,
-      infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1
     };
     return (
       <div>
-        <div>
-          <input type="text" onChange={this.handleChange.bind(this)}></input>
-        </div>
         <Slider {...settings}>
-          {this.state.slides.map(slide => {
-            return <div>
-              <img src={slide} />
+          {this.state.slides.map((slide, i) => {
+            return <div key={i}>
+              <img src={slide} alt="slide" style={styles} />
             </div>
           })}
         </Slider>
+        <div style={styles2}>
+          <input type="text" onChange={this.handleChange.bind(this)} style={styles3}></input>
+        </div>
       </div>
     );
   }
